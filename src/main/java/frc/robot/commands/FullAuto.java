29f85f117f6298d1fe6167c06b2d62f06478cd4c
Quinn Kehoe <kehoeq@gmail.com>
2026-03-21 13:29:35 -0500

    wayfinder implemented

---
+++ src/main/java/frc/robot/commands/FullAuto.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.commands.paths.DriveTrainConfig;
+import frc.robot.commands.paths.MultiPartPath;
+
+public class FullAuto extends SequentialCommandGroup {
+
+    public FullAuto() {
+        initializeCommands();
+    }
+
+    public void initializeCommands() {
+        // !WAYFINDER_INFO: {"trackWidth":0.864,"gameName":"Rebuilt"}
+        boolean doLastPart = SmartDashboard.getBoolean("DoLastPart?", true);
+        RobotContainer.drivetrain.resetAngle();
+
+        DriveTrainConfig config = RobotContainer.drivetrain.getConfig().makeClone();
+        config.maxVelocity = 4;
+        config.maxAcceleration = 4;
+        config.maxCentripetalAcceleration = 11;
+        config.maxAngularAcceleration = 8;
+        config.maxAnglularVelocity = 12;
+
+        MultiPartPath pathA = new MultiPartPath(RobotContainer.drivetrain, config, null);
+        pathA.resetPosition(0.381, 7.000);
+        pathA.addWaypoint(2.050, 7.000);
+        pathA.addWaypoint(11.928, 4.028);
+        if (doLastPart) {// path on
+            pathA.addWaypoint(4.430, 5.364);
+            pathA.addWaypoint(3.401, 5.027);
+        }
+        pathA.addStop();
+        addCommands(pathA.finalizePath());
+    }
+}
